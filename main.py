7b479a00b9108640ff741f7be5f311e23d4fd967
Laurie Chan Lam <laurie@lady-pony.com>
2021-05-15 09:53:26 +0200

    hello world

---
+++ main.py
@@ -0,0 +1,16 @@
+from collections import defaultdict
+import functools
+import itertools
+import json
+import math
+import numpy
+import re
+
+
+def print_hi(name):
+    print(f'Hi, {name}')
+
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
